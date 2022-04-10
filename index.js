bed3721a883d33d72a028266bf981d145ede8679
Asma <as1588ma@gmail.com>
2022-04-09 21:41:09 -0400

    complete my homework

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(drivers, name) {
+  return drivers.filter((driver) => {
+    return driver.toLowerCase() === name.toLowerCase();
+  });
+}
+function fuzzyMatch(drivers, target) {
+  return drivers.filter((driver) => {
+    return driver.startsWith(target);
+  });
+}
+function matchName(drivers, name) {
+  return drivers.filter((driver) => {
+    return driver.name === name;
+  });
+}
